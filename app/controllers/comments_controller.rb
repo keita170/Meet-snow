949b8936785a7ae45bb4ad3a8ab4896cb91101e1
keita170 <noinoi0565@gmail.com>
2021-09-07 03:43:35 +0000

    [add]favorite-desynchronization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,17 @@ class CommentsController < ApplicationController
 
   def create
     @student_post = StudentPost.find(params[:student_post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.student_post_id = @student_post.id
-    comment.save
+    @comment = current_user.comments.new(comment_params)
+    @comment.student_post_id = @student_post.id
+    @comment.save
+    #非同期化のためにコメントオフ
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @student_post = StudentPost.find(params[:student_post_id])
-    comment = Comment.find(params[:id])
-     #comment = current_user.comments.find_by(id: @booki.id)
-
+    # @student_post = StudentPost.find(params[:student_post_id])
+    # comment = Comment.find(params[:id])
+    comment = Comment.find_by(id: params[:id], student_post_id: params[:student_post_id])
     comment.destroy
     redirect_back(fallback_location: root_path)
   end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,8 +5,8 @@ class FavoritesController < ApplicationController
     @student_post = StudentPost.find(params[:student_post_id])
     @favorite = current_user.favorites.new(student_post_id: @student_post.id)
     @favorite.save
-    # @favorite = Favorite.create(user_id: current_user.id, student_post_id: @student_post.id)
-    redirect_back(fallback_location: root_path)
+    #非同期化のためにコメントアウトにする
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
@@ -14,7 +14,8 @@ class FavoritesController < ApplicationController
     @student_post = StudentPost.find(params[:student_post_id])
     @favorite = current_user.favorites.find_by(student_post_id: @student_post.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    #非同期化のためにコメントアウトにする
+    # redirect_back(fallback_location: root_path)
   end
 
 
