08b017cfa12f6cc9dcb522c2910375802d8724a8
keita170 <noinoi0565@gmail.com>
2021-09-21 06:30:49 +0000

    [add]mail-finaly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,14 +71,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    user_name: ENV['SEND_MAIL'],
-    password: ENV['SEND_MAIL_PASSWORD'],
-    authentication: :login,
-    openssl_verify_mode: 'none',
-    enable_starttls_auto: true
+    :address:"smtp.gmail.com",
+    :domain: 'gmail.com',
+    :port:587,
+    :user_name: ENV['SEND_MAIL'],
+    :password: ENV['SEND_MAIL_PASSWORD'],
+    :authentication: :login,
+    :openssl_verify_mode: 'none',
+    :enable_starttls_auto => true
   }
 
   config.action_mailer.perform_caching = false
