527f8e633b4f90afd95e8f1bdcab0ed98e61a4c7
keita170 <noinoi0565@gmail.com>
2021-09-22 06:51:50 +0000

    [add]production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,25 +65,33 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'アプリケーションのドメイン名'}
+  # config.action_mailer.default_url_options = { host: 'アプリケーションのドメイン名'}
 
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.gmail.com",
+  #   :domain => 'gmail.com',
+  #   :port => 587,
+  #   :user_name => ENV['SEND_MAIL'],
+  #   :password => ENV['SEND_MAIL_PASSWORD'],
+  #   :authentication => 'login',
+  #   :openssl_verify_mode => 'none',
+  #   :enable_starttls_auto => true
+  # }
+
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :domain => 'gmail.com',
-    :port => 587,
-    :user_name => ENV['SEND_MAIL'],
-    :password => ENV['SEND_MAIL_PASSWORD'],
-    :authentication => 'login',
-    :openssl_verify_mode => 'none',
-    :enable_starttls_auto => true
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['SEND_MAIL'],
+    password: ENV['SEND_MAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
-
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
