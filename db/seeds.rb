01fcfff81bba61ebc1af66415f8b61c0409c5c42
keita170 <noinoi0565@gmail.com>
2021-09-22 06:04:20 +0000

    [fix]

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create!(
-   email: ENV['ADMIN_EMAIL'],
-   password: ENV['ADMIN_PASSWORD']
+   email: 'meet@snow',
+   password: 'meetsnow'
 )
