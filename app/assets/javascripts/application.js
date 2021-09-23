6693bc0077d837c048f868e358c85a29eaf424e9
keita170 <noinoi0565@gmail.com>
2021-09-23 04:15:27 +0000

    [fix]phone-size

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,3 +35,10 @@
 /*global appendErrMsgToHTML*/
 /*global appendUser*/
 
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
